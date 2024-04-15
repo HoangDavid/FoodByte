22cab9364b10545f401cbb8e91bbbe68886f944b
Bendinh <bendinh0805@gmail.com>
2024-04-15 17:19:23 -0400

    [Navbar] Sidebar initialized

--- FoodByte/src/pages/Homepage.tsx
+++ FoodByte/src/pages/Homepage.tsx
@@ -1,16 +1,10 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export const Homepage = () => {
-    const navigate = useNavigate();
 
     return (
         <div style={{fontSize: '30px'}}>
             Welcome to FoodByte!
-            <div>
-                <button onClick={() => navigate('/fridge')}>Fridge</button>
-                <button onClick={() => navigate('/recipe')}>Recipe</button>
-                <button onClick={() => navigate('/shopping')}>Shopping</button>
-            </div>
         </div>
     );
 };
